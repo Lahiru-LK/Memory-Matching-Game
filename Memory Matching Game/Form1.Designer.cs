b42d837407b04f9484b7f6ca12d86891d3bb97bb
Lahiru Prasad <104630433+Lahiru-LK@users.noreply.github.com>
2024-08-01 18:13:22 +0530

    Add files via upload

---
+++ Memory Matching Game/Form1.Designer.cs
@@ -0,0 +1,411 @@
+﻿namespace Memory_Matching_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            label1 = new Label();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            pictureBox9 = new PictureBox();
+            pictureBox10 = new PictureBox();
+            pictureBox11 = new PictureBox();
+            pictureBox12 = new PictureBox();
+            pictureBox13 = new PictureBox();
+            label4 = new Label();
+            label5 = new Label();
+            pictureBox14 = new PictureBox();
+            pictureBox15 = new PictureBox();
+            pictureBox16 = new PictureBox();
+            pictureBox17 = new PictureBox();
+            pictureBox18 = new PictureBox();
+            pictureBox19 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox14).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox15).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox16).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox17).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox18).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox19).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Image = Properties.Resources.Space_Memory_Game_Worksheet;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(452, 651);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 192, 192);
+            button1.BackgroundImageLayout = ImageLayout.Center;
+            button1.Font = new Font("Showcard Gothic", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.IndianRed;
+            button1.Location = new Point(121, 531);
+            button1.Name = "button1";
+            button1.Size = new Size(210, 50);
+            button1.TabIndex = 1;
+            button1.Text = "Restart";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BorderStyle = BorderStyle.Fixed3D;
+            label1.Cursor = Cursors.No;
+            label1.FlatStyle = FlatStyle.Popup;
+            label1.Location = new Point(177, 497);
+            label1.Name = "label1";
+            label1.Size = new Size(104, 22);
+            label1.TabIndex = 2;
+            label1.Text = "Time Left : 30 ";
+            label1.Click += label1_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.Memory_Game_;
+            pictureBox2.Location = new Point(99, 112);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(82, 73);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.Memory_Game_;
+            pictureBox3.Location = new Point(275, 112);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(82, 73);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 4;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.Memory_Game_;
+            pictureBox4.Location = new Point(187, 112);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(82, 73);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 5;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = Properties.Resources.Memory_Game_;
+            pictureBox5.Location = new Point(187, 191);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(82, 73);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 8;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = Properties.Resources.Memory_Game_;
+            pictureBox6.Location = new Point(275, 191);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(82, 73);
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.TabIndex = 7;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Image = Properties.Resources.Memory_Game_;
+            pictureBox7.Location = new Point(99, 191);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(82, 73);
+            pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox7.TabIndex = 6;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Image = Properties.Resources.Memory_Game_;
+            pictureBox8.Location = new Point(187, 351);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(82, 73);
+            pictureBox8.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox8.TabIndex = 14;
+            pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.Image = Properties.Resources.Memory_Game_;
+            pictureBox9.Location = new Point(275, 351);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(82, 73);
+            pictureBox9.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox9.TabIndex = 13;
+            pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            pictureBox10.Image = Properties.Resources.Memory_Game_;
+            pictureBox10.Location = new Point(99, 351);
+            pictureBox10.Name = "pictureBox10";
+            pictureBox10.Size = new Size(82, 73);
+            pictureBox10.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox10.TabIndex = 12;
+            pictureBox10.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            pictureBox11.Image = Properties.Resources.Memory_Game_;
+            pictureBox11.Location = new Point(187, 272);
+            pictureBox11.Name = "pictureBox11";
+            pictureBox11.Size = new Size(82, 73);
+            pictureBox11.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox11.TabIndex = 11;
+            pictureBox11.TabStop = false;
+            // 
+            // pictureBox12
+            // 
+            pictureBox12.Image = Properties.Resources.Memory_Game_;
+            pictureBox12.Location = new Point(275, 272);
+            pictureBox12.Name = "pictureBox12";
+            pictureBox12.Size = new Size(82, 73);
+            pictureBox12.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox12.TabIndex = 10;
+            pictureBox12.TabStop = false;
+            // 
+            // pictureBox13
+            // 
+            pictureBox13.Image = Properties.Resources.Memory_Game_;
+            pictureBox13.Location = new Point(99, 272);
+            pictureBox13.Name = "pictureBox13";
+            pictureBox13.Size = new Size(82, 73);
+            pictureBox13.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox13.TabIndex = 9;
+            pictureBox13.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BorderStyle = BorderStyle.Fixed3D;
+            label4.Cursor = Cursors.No;
+            label4.FlatStyle = FlatStyle.Popup;
+            label4.Location = new Point(105, 466);
+            label4.Name = "label4";
+            label4.Size = new Size(46, 22);
+            label4.TabIndex = 17;
+            label4.Text = "Life : ";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BorderStyle = BorderStyle.Fixed3D;
+            label5.Cursor = Cursors.No;
+            label5.FlatStyle = FlatStyle.Popup;
+            label5.Location = new Point(105, 436);
+            label5.Name = "label5";
+            label5.Size = new Size(63, 22);
+            label5.TabIndex = 18;
+            label5.Text = "Point : 0";
+            label5.Click += label5_Click;
+            // 
+            // pictureBox14
+            // 
+            pictureBox14.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox14.Image = Properties.Resources.hart;
+            pictureBox14.Location = new Point(157, 466);
+            pictureBox14.Name = "pictureBox14";
+            pictureBox14.Size = new Size(20, 20);
+            pictureBox14.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox14.TabIndex = 19;
+            pictureBox14.TabStop = false;
+            // 
+            // pictureBox15
+            // 
+            pictureBox15.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox15.Image = Properties.Resources.hart;
+            pictureBox15.Location = new Point(183, 466);
+            pictureBox15.Name = "pictureBox15";
+            pictureBox15.Size = new Size(20, 20);
+            pictureBox15.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox15.TabIndex = 20;
+            pictureBox15.TabStop = false;
+            // 
+            // pictureBox16
+            // 
+            pictureBox16.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox16.Image = Properties.Resources.hart;
+            pictureBox16.Location = new Point(235, 466);
+            pictureBox16.Name = "pictureBox16";
+            pictureBox16.Size = new Size(20, 20);
+            pictureBox16.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox16.TabIndex = 22;
+            pictureBox16.TabStop = false;
+            // 
+            // pictureBox17
+            // 
+            pictureBox17.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox17.Image = Properties.Resources.hart;
+            pictureBox17.Location = new Point(209, 466);
+            pictureBox17.Name = "pictureBox17";
+            pictureBox17.Size = new Size(20, 20);
+            pictureBox17.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox17.TabIndex = 21;
+            pictureBox17.TabStop = false;
+            // 
+            // pictureBox18
+            // 
+            pictureBox18.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox18.Image = (Image)resources.GetObject("pictureBox18.Image");
+            pictureBox18.Location = new Point(284, 466);
+            pictureBox18.Name = "pictureBox18";
+            pictureBox18.Size = new Size(20, 20);
+            pictureBox18.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox18.TabIndex = 24;
+            pictureBox18.TabStop = false;
+            // 
+            // pictureBox19
+            // 
+            pictureBox19.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox19.Image = Properties.Resources.hart;
+            pictureBox19.Location = new Point(258, 466);
+            pictureBox19.Name = "pictureBox19";
+            pictureBox19.Size = new Size(20, 20);
+            pictureBox19.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox19.TabIndex = 23;
+            pictureBox19.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.GhostWhite;
+            ClientSize = new Size(452, 651);
+            Controls.Add(pictureBox18);
+            Controls.Add(pictureBox19);
+            Controls.Add(pictureBox16);
+            Controls.Add(pictureBox17);
+            Controls.Add(pictureBox15);
+            Controls.Add(pictureBox14);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox9);
+            Controls.Add(pictureBox10);
+            Controls.Add(pictureBox11);
+            Controls.Add(pictureBox12);
+            Controls.Add(pictureBox13);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            ForeColor = Color.IndianRed;
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Memory Matching Game";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox14).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox15).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox16).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox17).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox18).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox19).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Label label1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBox10;
+        private PictureBox pictureBox11;
+        private PictureBox pictureBox12;
+        private PictureBox pictureBox13;
+        private Label label4;
+        private Label label5;
+        private PictureBox pictureBox14;
+        private PictureBox pictureBox15;
+        private PictureBox pictureBox16;
+        private PictureBox pictureBox17;
+        private PictureBox pictureBox18;
+        private PictureBox pictureBox19;
+    }
+}
---
+++ Memory Matching Game/Form1.cs
@@ -0,0 +1,45 @@
+namespace Memory_Matching_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
